87046ae3998ed8fef244963507e355b845792958
Tiago Silvino <tssdj13@gmail.com>
2026-03-12 11:56:01 -0300

    feat(core): add NotFoundError class

---
+++ api/src/core/errors/NotFoundError.ts
@@ -0,0 +1,8 @@
+import { AppError } from "./AppError";
+
+export class NotFoundError extends AppError {
+  constructor(message: string = "Not Found") {
+    super(message, 404);
+    this.name = "NotFoundError";
+  }
+}
