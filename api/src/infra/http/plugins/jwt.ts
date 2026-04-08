17b85b4d7eccce7a40c65ab410e1f5ee3958dff0
Tiago Silvino <tssdj13@gmail.com>
2026-04-08 09:45:07 -0300

    feat(auth): add jwt plugin with authenticate and requireAdmin decorators

--- api/src/infra/http/plugins/jwt.ts
+++ api/src/infra/http/plugins/jwt.ts
@@ -3,10 +3,15 @@ import type { FastifyInstance } from 'fastify'
 import fp from 'fastify-plugin'
 import { env } from '@/config/env'
 
+import { authenticate, requireAdmin } from '../hooks/authenticate'
+
 async function jwtPlugin(app: FastifyInstance) {
-  app.register(fastifyJwt, {
+  await app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
   })
+
+  app.decorate('authenticate', authenticate)
+  app.decorate('requireAdmin', requireAdmin)
 }
 
 export default fp(jwtPlugin)
